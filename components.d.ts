36d90218a2e4d97b4eb90c7d066f39ed81ff90be
赵乐乐 <zhaolele@51shebao.com>
2023-10-08 12:46:04 +0800

    feature(custom): some

--- components.d.ts
+++ components.d.ts
@@ -7,9 +7,9 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
+    ElButton: (typeof import('element-plus/es'))['ElButton'];
+    HelloWorld: (typeof import('./src/components/HelloWorld.vue'))['default'];
+    RouterLink: (typeof import('vue-router'))['RouterLink'];
+    RouterView: (typeof import('vue-router'))['RouterView'];
   }
 }
